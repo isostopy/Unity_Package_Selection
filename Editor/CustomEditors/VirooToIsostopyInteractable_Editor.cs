2a84083ec7fe174f02e6158a7cef97bfd273e369
Sergio <software@isostopy.com>
2025-08-21 13:30:23 +0200

    Añadido VirooToIsostopyInteractable
    Una clase que trasmite los eventos del interactuable de Viroo al de Isostopy.

---
+++ Editor/CustomEditors/VirooToIsostopyInteractable_Editor.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Isostopy.Selection.Viroo.Editor
+{
+
+	[CustomEditor(typeof(VirooToIsostopyInteractable))]
+	public class VirooToIsostopyInteractable_Editor : UnityEditor.Editor
+	{
+#if !VIROO
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			EditorGUILayout.Space();
+			EditorGUILayout.HelpBox(
+				"Este componente necesita tener importado el paquete de Viroo y haber añadido el scripting define symbol VIROO en las player settings.\n" +
+				"Asegurate de haber añadido el paquete de viroo y pulsa el boton para añadir el define a las player settings.",
+				MessageType.Info);
+			if (GUILayout.Button("#define VIROO"))
+			{
+				PlayerSettings.SetScriptingDefineSymbols(UnityEditor.Build.NamedBuildTarget.Standalone, "VIROO");
+			}
+		}
+#endif
+	}
+}
---
+++ Runtime/Viroo/VirooToIsostopyInteractable.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+
+namespace Isostopy.Selection.Viroo
+{
+
+#if VIROO
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit;
+#endif
+
+	/// <summary>
+	/// Componente que trasmite los eventos desde un interactuable de Viroo a uno de Isostopy. </summary>
+#if VIROO
+[RequireComponent (typeof(PointerInteractable))]
+[RequireComponent (typeof(XRSimpleInteractable))]
+#endif
+	public class VirooToIsostopyInteractable : MonoBehaviour
+	{
+#if VIROO
+
+	PointerInteractable isostopyInteractable = null;
+	XRSimpleInteractable virooInteractable = null;
+
+	bool pressed = false;
+
+
+	// ----------------------------------------------------------------------------
+
+	private void Awake()
+	{
+		isostopyInteractable = GetComponent<PointerInteractable>();
+		virooInteractable = GetComponent<XRSimpleInteractable>();
+
+		virooInteractable.hoverEntered.AddListener(VirooHoverEnter);
+		virooInteractable.hoverExited.AddListener(VirooHoverExit);
+		//virooInteractable.activated.AddListener(VirooActivate);
+		//virooInteractable.deactivated.AddListener(VirooDeactivate);
+		/* DESCOMENTAR ESTO CUANDO LO ARREGLEN DE VIROO.
+			La ultima vez que probamos añadir el listener en cogido funcionaba en local
+			pero los eventos de que se habia hecho clic no se propagaban en red.
+			Si añades los listeler a mano en el editor si que funciona en red, 
+			asi que de momento hemos añadido un clic derecho -> añadir listeners que facilite el trabajo pero que no es ideal. */
+	}
+
+
+	// ----------------------------------------------------------------------------
+
+	private void VirooHoverEnter(HoverEnterEventArgs args)
+	{
+		isostopyInteractable.HoverEnter();
+	}
+
+	private void VirooHoverExit(HoverExitEventArgs args)
+	{
+		isostopyInteractable.HoverExit();
+
+		if (pressed)
+		{
+			pressed = false;
+			isostopyInteractable.PressUp();
+			// Al sacar el raton del objeto hacemos como que el usuario al levantado el boton.
+			// Porque cuando se levanta el boton sin estar apuntado a este objeto, Viroo no nos notifica.
+		}
+	}
+
+	private void VirooActivate(ActivateEventArgs args)
+	{
+		// Esta funcion se llama cuando se hace click sobre el objeto.
+
+		isostopyInteractable.PressDown();
+		pressed = true;
+	}
+
+	private void VirooDeactivate(DeactivateEventArgs args)
+	{
+		// Esta funcion solo se llama cuando se suelta el raton sobre el objeto.
+		// Aunque se haya hecho click sobre el objeto, si luego se suelta el boton fuera del objeto, no se llama.
+
+		if (pressed)
+		{
+			pressed = false;
+			isostopyInteractable.PressUp();
+			isostopyInteractable.Click();
+		}
+	}
+	
+
+	// -------------------------------------------------------------------------------------
+
+#if UNITY_EDITOR
+	[ContextMenu("ISOSTOPY - Add Listeneres to XRSimpleInteractable")]
+	private void AddListenerToViroo()
+	{
+		var xrSimpleInteractable = GetComponent<XRSimpleInteractable>();
+
+		UnityEditor.Undo.RecordObject(xrSimpleInteractable, "Añadidos listener de Viroo");
+
+		UnityEditor.Events.UnityEventTools.AddPersistentListener(xrSimpleInteractable.activated, VirooActivate);
+		UnityEditor.Events.UnityEventTools.AddPersistentListener(xrSimpleInteractable.deactivated, VirooDeactivate);
+	}
+#endif
+
+#endif
+	}
+}
+
