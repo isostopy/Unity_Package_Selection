cff8f68268b605695b2b41c8dcc09b6970c73bf8
Sergio <software@isostopy.com>
2025-12-17 11:52:17 +0100

    Erratas

--- Editor/CustomEditors/VirooToIsostopyInteractable_Editor.cs
+++ Editor/CustomEditors/VirooToIsostopyInteractable_Editor.cs
@@ -14,7 +14,7 @@ namespace Isostopy.Selection.Viroo.Editor
 
 			EditorGUILayout.Space();
 			EditorGUILayout.HelpBox(
-				"Este componente necesita tener importado el paquete de Viroo y haber añadido el scripting define symbol VIROO en las player settings.\n" +
+				"Este componente necesita tener importado el paquete de Viroo y haber añadido el scripting define symbol \"VIROO\" en las player settings.\n" +
 				"Asegurate de haber añadido el paquete de viroo y pulsa el boton para añadir el define a las player settings.",
 				MessageType.Info);
 			if (GUILayout.Button("#define VIROO"))
